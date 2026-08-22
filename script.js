2ed4a5fdaca62940160884bbcbf0de73e17c2f94
aliraza-glitch <aliraza9027181@gmail.com>
2026-08-22 18:16:27 +0500

    Implement bot message responses in chat
    Added bot response functionality to the chat application, including case-insensitive message handling and predefined responses for specific user inputs.

--- script.js
+++ script.js
@@ -7,9 +7,37 @@ userMsg.className = "UserBubble"
 userMsg.textContent = Message
 newMsg.append(userMsg)
 input.value = ""
+let caseInsensitive = Message.toLowerCase()
+sendBotMessage(caseInsensitive)
 }
 input.addEventListener("keydown",(enter) => {
     if (enter.key == "Enter"){
         sendMessage();
     }
 })
+function sendBotMessage(Userinput){
+    let Botmsg = document.createElement("div")
+    Botmsg.className = "Botmsg"
+    Botmsg.textContent = "Hey Great Question!. Let's work that out together"
+    let findChat = document.querySelector(".Chat")
+    findChat.append(Botmsg)
+        let answer = getResponse(Userinput)
+        Botmsg.textContent = answer.join("\n")
+}
+
+function getResponse(Userinput){
+    let responses = []
+    if(Userinput.includes("hello")){
+        responses.push("Hey! I started thinking you would make me do all the studying by myself.😏")
+    }
+    if (Userinput.includes("momentum")){
+        responses.push("Momentum is the product of mass and velocity of a body")
+    }
+    if (Userinput.includes("acceleration")){
+        responses.push("Acceleration is the rate of change of velocity")
+    }
+    if (responses.length === 0){
+        responses.push("How about we open a textbook for that one? 😭📚")
+    }
+    return responses
+}
