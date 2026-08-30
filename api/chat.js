8116775703f03ca141d4996013e190c8811abcd4
aliraza-glitch <aliraza9027181@gmail.com>
2026-08-30 13:10:21 +0500

    Use Groq AI response

--- api/chat.js
+++ api/chat.js
@@ -24,21 +24,14 @@ const response = await fetch("https://api.groq.com/openai/v1/chat/completions" ,
 })
 const data = await response.json()
 console.log (data)
-
+if (data.error){
+    return res.status(500).json({
+        error: data.error.message
+    })
+}
 let Replydata = {
-    Reply: "I have recieved your message"
+    Reply: data.choices[0].message.content
 }
-if (message.includes("momentum")){
-    Replydata.Reply = "Momentum is the product of mass and velocity of an object"
-}
-if (message.includes("acceleration")){
-    Replydata.Reply = "Acceleration is the rate of change of velocity"
-}
-if (message.includes("hello")){
-    Replydata.Reply = "Hey! Look who showed up"
-}
-if (message.includes("what")){
-    Replydata.Reply = "You are using AI ,the pinnacle of human technology, to get an answer to a question like that "
-}
+
 res.status(200).json(Replydata)
 }
